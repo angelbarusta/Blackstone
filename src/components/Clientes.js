50d80a347e93d4445c76e370c83f9078a8ab4718
Angel Barusta <angel.barusta.95@gmail.com>
2020-04-11 01:11:20 -0500

    master blackstone

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -5,22 +5,39 @@ import { Card, Image, List, Tab } from "semantic-ui-react";
 import "../pages/styles/Home.css";
 
 const show1 = {
-  ima: faker.image.image(),
+  ima: faker.internet.avatar(),
   desc: faker.name.jobArea(),
   name: faker.internet.userName(),
 };
 const show2 = {
-  ima: faker.image.image(),
+  ima: faker.internet.avatar(),
   desc: faker.name.jobArea(),
   name: faker.internet.userName(),
 };
 const show3 = {
-  ima: faker.image.image(),
+  ima: faker.internet.avatar(),
+  desc: faker.name.jobArea(),
+  name: faker.internet.userName(),
+};
+
+const show4 = {
+  ima: faker.internet.avatar(),
+  desc: faker.name.jobArea(),
+  name: faker.internet.userName(),
+};
+const show5 = {
+  ima: faker.internet.avatar(),
+  desc: faker.name.jobArea(),
+  name: faker.internet.userName(),
+};
+const show6 = {
+  ima: faker.internet.avatar(),
   desc: faker.name.jobArea(),
   name: faker.internet.userName(),
 };
 
 const lista = [show1, show2, show3];
+const listaNueva = [show4, show5, show6];
 
 console.log("lista :", lista);
 
@@ -30,7 +47,22 @@ const Clientes = () => {
       <Image
         circular
         className='HOME__Clientes_ListCardItem_Image'
-        src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+        src={ima}
+        //src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+      />
+      <div className='HOME__Clientes_ListCardItem_Image_desc'>
+        <List.Header>{name}</List.Header>
+        {desc}
+      </div>
+    </List.Item>
+  ));
+  const LIST_CARD_NUEVA = listaNueva.map(({ ima, desc, name }, i) => (
+    <List.Item key={i} className='HOME__Clientes_ListCardItem'>
+      <Image
+        circular
+        className='HOME__Clientes_ListCardItem_Image'
+        src={ima}
+        // src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
       />
       <div className='HOME__Clientes_ListCardItem_Image_desc'>
         <List.Header>{name}</List.Header>
@@ -40,7 +72,7 @@ const Clientes = () => {
   ));
   const panes = [
     {
-      menuItem: "Tab 1",
+      menuItem: "NEW CLIENTS",
       render: () => (
         <List Horizontal className='HOME__Clientes_ListCardAnnou'>
           {LIST_CARD}
@@ -48,8 +80,12 @@ const Clientes = () => {
       ),
     },
     {
-      menuItem: "Tab 2",
-      render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane>,
+      menuItem: "NEW STAFF",
+      render: () => (
+        <List Horizontal className='HOME__Clientes_ListCardAnnou'>
+          {LIST_CARD_NUEVA}
+        </List>
+      ),
     },
   ];
   return (
